4a9b8e9b7a07d93463bdc4334ce744872b73dfa8
Sherif <sherifnabil441@gmail.com>
2026-03-30 14:58:58 +0200

    Option API routes

--- survey-service/routes/api.php
+++ survey-service/routes/api.php
@@ -17,11 +17,23 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('/', [\App\Http\Controllers\API\SectionController::class, 'index']);
         Route::post('/', [\App\Http\Controllers\API\SectionController::class, 'store']);
     });
-
     // Individual Section routes
     Route::group(['prefix' => 'sections'], function () {
         Route::get('{section}', [\App\Http\Controllers\API\SectionController::class, 'show']);
         Route::put('{section}', [\App\Http\Controllers\API\SectionController::class, 'update']);
         Route::delete('{section}', [\App\Http\Controllers\API\SectionController::class, 'destroy']);
+    });
+
+    // Survey Options - Nested routes under surveys
+    Route::group(['prefix' => 'surveys/{survey}/options'], function () {
+        Route::get('/', [\App\Http\Controllers\API\OptionController::class, 'index']);
+        Route::post('/', [\App\Http\Controllers\API\OptionController::class, 'store']);
+    });
+    // Individual Option routes
+    Route::group(['prefix' => 'options'], function () {
+        Route::get('types', [\App\Http\Controllers\API\OptionController::class, 'types']);
+        Route::get('{option}', [\App\Http\Controllers\API\OptionController::class, 'show']);
+        Route::put('{option}', [\App\Http\Controllers\API\OptionController::class, 'update']);
+        Route::delete('{option}', [\App\Http\Controllers\API\OptionController::class, 'destroy']);
     });
 });
