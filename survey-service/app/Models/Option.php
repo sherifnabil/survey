deb4a279725fe36534cc0e79d9de1d41290b2d0a
Sherif <sherifnabil441@gmail.com>
2026-03-30 14:55:32 +0200

    Added filter scope for filtering results based on given request criteria

--- survey-service/app/Models/Option.php
+++ survey-service/app/Models/Option.php
@@ -31,4 +31,21 @@ class Option extends Model
     {
         return $this->hasMany(Answer::class);
     }
+
+    public function scopeFilters($query, array $filters): void
+    {
+        $surveyId = $filters['survey_id'] ?? null;
+        $name = $filters['name'] ?? null;
+        $type = $filters['type'] ?? null;
+
+        $query->when($surveyId, function ($query, $surveyId) {
+            $query->where('survey_id', $surveyId);
+        })
+            ->when($name ?? null, function ($query, $name) {
+                $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->when($type ?? null, function ($query, $type) {
+                $query->where('type', $type);
+            });
+    }
 }
